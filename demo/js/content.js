cbff86cd15e7204f48f2b826f0f818aaa6e8a5e7
alactic <okaforechezona1992@gmail.com>
2023-02-10 20:02:10 +0100

    fixing the env variables

--- demo/js/content.js
+++ demo/js/content.js
@@ -1,5 +1,5 @@
 function initializatonWidget(){
-    console.log({windowEnv3: window})
+    console.log({windowEnv3: window.env})
     document.getElementById( 'widget-payment-container' ).style.display = 'none';
     document.getElementById( 'transfer-container-495gjjhg-gkhkhjg' ).style.display = 'flex';
 }
