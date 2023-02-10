f929e5217c0dd3f0a62ec4d9064f49455445bf18
alactic <okaforechezona1992@gmail.com>
2023-02-10 20:01:21 +0100

    fixing the env variables

--- demo/js/content.js
+++ demo/js/content.js
@@ -1,11 +1,11 @@
 function initializatonWidget(){
-    console.log({windowEnv2: window.env})
+    console.log({windowEnv3: window})
     document.getElementById( 'widget-payment-container' ).style.display = 'none';
     document.getElementById( 'transfer-container-495gjjhg-gkhkhjg' ).style.display = 'flex';
 }
 
 function initializeBankPayment(){
-    console.log({windowEnv2: window.env})
+    console.log({windowEnv3: window})
     document.getElementById( 'transfer-container-495gjjhg-gkhkhjg' ).style.display = 'none';
     document.getElementById( 'atm-card-container-iitg33405-fgti594' ).style.display = 'flex';
     document.getElementById( 'widget-payment-container' ).style.display = 'flex';
