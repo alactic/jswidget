57fb9a37cf1bc8f51545867a493bef94fdf546cc
alactic <okaforechezona1992@gmail.com>
2023-03-31 09:43:21 +0100

    switching base url to test

--- demo/js/initialize.js
+++ demo/js/initialize.js
@@ -9,8 +9,8 @@ var BaseApiUrl = "";
 
 
     if(window.location.origin.indexOf('localhost') > -1 || window.location.origin.indexOf('baseonecollectwidgettest') > -1){
-        BaseApiUrl = "https://afcollectionaggregatorprod.azurewebsites.net/v1";
-        // BaseApiUrl = "https://afcollectionaggregatortest.azurewebsites.net/v1";
+        // BaseApiUrl = "https://afcollectionaggregatorprod.azurewebsites.net/v1";
+        BaseApiUrl = "https://afcollectionaggregatortest.azurewebsites.net/v1";
     }else if(
      window.location.origin.indexOf('baseonecollectwidgetstaging') > -1){
         BaseApiUrl = "https://collectioncardservice-stag.azurewebsites.net/v1";
