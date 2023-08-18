41c139f18aea00605acaff4b103d0662a9318406
alactic <okaforechezona1992@gmail.com>
2023-08-18 07:46:35 +0100

    fixing qa issue

--- demo/js/validatePayment.js
+++ demo/js/validatePayment.js
@@ -13,48 +13,71 @@ function validatePayment(type){
     }
     document.getElementById("transaction-loading-container-fgti594").style.display="flex";
     document.getElementById( 'bank-transfer-iitg33405-fgti594' ).style.display = 'none';
-
-    fetch(validateUrl, {
-    method: 'POST',
-    headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    "Public-Key": publicKey
-    },
-    body: null
-    })
-    .then(response => response.json())
-    .then(res => {
-        const status = res.responseData && res.responseData.transactionStatus;
-        if(res.responseData && status !=="Pending" && status !== "Processing" && status !== "Success"){
-                document.getElementById("transaction-message-fgti594").style.display="flex"
-                document.getElementById("transaction-items").style.display="none"
-                document.getElementById("transaction-failed-items").style.display="flex"
-                document.getElementsByClassName("message-desc")[0].innerHTML=res.message==="Successful"?"Failed":res.message;
-                const url = new URL(window.location.href);
-                document.getElementById("transaction-loading-container-fgti594").style.display="none"
-
-            }else if((status !== "Processing" &&  status !=="Pending") || (type === "card" && status === "Processing")){
-        console.log({s1:status})
-        document.getElementById("transaction-message-fgti594").style.display="flex"
-                document.getElementById("transaction-items").style.display="flex"
-                document.getElementById("transaction-failed-items").style.display="none"
-                document.getElementsByClassName("amount")[0].innerHTML=res.message
-                document.getElementsByClassName("currency")[0].innerHTML=res.message
-                document.getElementById("bank-name").innerHTML=res.message
-                document.getElementById("account-number").innerHTML=res.message
-                document.getElementsByClassName("message-desc")[0].innerHTML=res.message;
-                const url = new URL(window.location.href);
-                // const successRes = url.searchParams.get("success");
-                // const onSuccess = eval("(" + successRes + ")");
-                // onSuccess()
-            document.getElementById("transaction-loading-container-fgti594").style.display="none"
-        }else{
-           setTimeout(() => validatePayment(), 15000)
-        }
+    document.getElementById("transaction-counter").style.display="flex";
     
-   }).catch(error => {
-       console.log({error});
-   })
+    const counterInterval = setInterval(()=> {
+        let counter = document.getElementById("transaction-counter-second").innerHTML;
+        document.getElementById("transaction-counter-second").innerHTML = Number(counter) - 1;
+        if(Number(counter) === 1) {
+            document.getElementById("transaction-counter-second").innerHTML =  59
+            let counterMin = document.getElementById("transaction-counter-digit").innerHTML;
+            if(Number(counterMin) === 0) {
+                document.getElementById("transaction-message").style.display="flex";
+                document.getElementById("transaction-feedback-close-btn-div").style.display="flex";
+                document.getElementById("transaction-counter").style.display="none";
+                document.getElementById("loader-content").style.display="none";
+            }else {
+                document.getElementById("transaction-counter-digit").innerHTML = Number(counterMin) - 1;
+            }
+        }
+    }, 1000)
+   
+    let makeValidationInterval
+    const makeValidation = async() =>{
+        fetch(validateUrl, {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            "Public-Key": publicKey
+            },
+            body: null
+            })
+            .then(response => response.json())
+            .then(res => {
+                const status = res.responseData && res.responseData.transactionStatus;
+                if(res.responseData && status !=="Pending" && status !== "Processing" && status !== "Success"){
+                        document.getElementById("transaction-message-fgti594").style.display="flex"
+                        document.getElementById("transaction-items").style.display="none"
+                        document.getElementById("transaction-failed-items").style.display="flex"
+                        document.getElementsByClassName("message-desc")[0].innerHTML=res.message==="Successful"?"Failed":res.message;
+                        const url = new URL(window.location.href);
+                        document.getElementById("transaction-loading-container-fgti594").style.display="none"
+                        clearInterval(counterInterval)
+                        clearInterval(makeValidationInterval)
+                        
+                    }else if((status !== "Processing" &&  status !=="Pending") || (type === "card" && status === "Processing")){
+                document.getElementById("transaction-message-fgti594").style.display="flex"
+                        document.getElementById("transaction-items").style.display="flex"
+                        document.getElementById("transaction-failed-items").style.display="none"
+                        document.getElementsByClassName("amount")[0].innerHTML=res.message
+                        document.getElementsByClassName("currency")[0].innerHTML=res.message
+                        document.getElementById("bank-name").innerHTML=res.message
+                        document.getElementById("account-number").innerHTML=res.message
+                        document.getElementsByClassName("message-desc")[0].innerHTML=res.message;
+                        const url = new URL(window.location.href);
+                        // const successRes = url.searchParams.get("success");
+                        // const onSuccess = eval("(" + successRes + ")");
+                        // onSuccess()
+                    document.getElementById("transaction-loading-container-fgti594").style.display="none"
+                }else{
+                    makeValidationInterval = setTimeout(() => makeValidation(), 15000)
+                }
+            
+           }).catch(error => {
+               console.log({error});
+           })
+    }
+    makeValidation()
        
 }
